c781bc45bde5d81585fb141d4bed7c2984ccbbcc
HaluHalu <huska696@gmail.com>
2019-10-28 21:01:27 +0700

    dasdsak

--- app/src/main/java/com/example/newworldphotoeditor/MainActivity.java
+++ app/src/main/java/com/example/newworldphotoeditor/MainActivity.java
@@ -1,14 +1,66 @@
 package com.example.newworldphotoeditor;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Camera;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_ID_READ_WRITE_PERMISSION = 99;
+    private static final int REQUEST_ID_IMAGE_CAPTURE =100 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onCollage(View view) {
+    }
+
+    public void onCamera(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // Start Activity chụp hình, và chờ đợi kết quả trả về.
+        this.startActivityForResult(intent, REQUEST_ID_IMAGE_CAPTURE);
+
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //
+        switch (requestCode) {
+            case REQUEST_ID_READ_WRITE_PERMISSION: {
+                if (grantResults.length > 1
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+
+                    Toast.makeText(this, "Permission granted!", Toast.LENGTH_LONG).show();
+
+//                    this.captureVideo();
+
+                }
+                else {
+                    Toast.makeText(this, "Permission denied!", Toast.LENGTH_LONG).show();
+                }
+                break;
+            }
+        }
+    }
+
+    public void onGallery(View view) {
     }
 }
