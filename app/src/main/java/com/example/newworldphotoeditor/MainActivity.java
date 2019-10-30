268190954b4b84822a8b22ff5916a596f1438ced
Khoaj <khoaiboy410@gmail.com>
2019-10-30 15:02:47 +0700

    Camera + Gallery Activity

--- app/src/main/java/com/example/newworldphotoeditor/MainActivity.java
+++ app/src/main/java/com/example/newworldphotoeditor/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.newworldphotoeditor;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -16,10 +18,12 @@ import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final int REQUEST_ID_READ_WRITE_PERMISSION = 99;
-    private static final int REQUEST_ID_IMAGE_CAPTURE =100 ;
-
+    private static final int GALLERY_RESULT = 1;
+    private static final int CAMERA_RESULT = 2;
+    private static final String FILE_PROVIDER_AUTHORITY = "com.example.newworldphotoeditor";
+    private static final int CAMERA_PERMISSION_CODE = 99;
+    private static final int STORAGE_PERMISSION_CODE = 100 ;
+    private String mCaptureImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,40 +31,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onCollage(View view) {
+
     }
 
     public void onCamera(View view) {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-        // Start Activity chụp hình, và chờ đợi kết quả trả về.
-        this.startActivityForResult(intent, REQUEST_ID_IMAGE_CAPTURE);
-
-    }
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        //
-        switch (requestCode) {
-            case REQUEST_ID_READ_WRITE_PERMISSION: {
-                if (grantResults.length > 1
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED
-                        && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
-
-                    Toast.makeText(this, "Permission granted!", Toast.LENGTH_LONG).show();
-
-//                    this.captureVideo();
-
-                }
-                else {
-                    Toast.makeText(this, "Permission denied!", Toast.LENGTH_LONG).show();
-                }
-                break;
-            }
-        }
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_RESULT);
     }
 
     public void onGallery(View view) {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), GALLERY_RESULT);
     }
 }
