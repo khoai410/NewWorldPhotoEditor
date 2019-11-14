7a02baaa4dece119270be2769005f76599f55664
HaluHalu <huska696@gmail.com>
2019-11-14 12:57:59 +0700

    login facebook

---
+++ app/src/main/java/com/example/newworldphotoeditor/LoginFacebookActivity.java
@@ -0,0 +1,89 @@
+package com.example.newworldphotoeditor;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.facebook.login.widget.ProfilePictureView;
+
+import org.json.JSONObject;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class LoginFacebookActivity extends AppCompatActivity {
+    private ProfilePictureView friendProfilePicture;
+    private TextView txtUsernameFacebook;
+    private LoginButton btnLoginFacebook;
+    private CallbackManager callbackManager;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager=CallbackManager.Factory.create();
+
+        setContentView(R.layout.activity_login_facebook);
+        init();
+        btnLoginFacebook.setReadPermissions(Arrays.asList("public_profile","email"));
+        onLoginFacebook();
+    }
+    public void onLoginFacebook(){
+        btnLoginFacebook.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                result();
+btnLoginFacebook.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+    }
+
+    private void result() {
+        GraphRequest graphRequest=GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+                Log.e("JSON",response.getJSONObject().toString());
+            }
+        });
+        Bundle params=new Bundle();
+        params.putString("Halu","name");
+        graphRequest.setParameters(params);
+        graphRequest.executeAsync();
+    }
+
+    public void init(){
+        friendProfilePicture = (ProfilePictureView) findViewById(R.id.friendProfilePicture);
+        txtUsernameFacebook = (TextView) findViewById(R.id.txt_username_facebook);
+        btnLoginFacebook = (LoginButton) findViewById(R.id.btn_login_facebook);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+}
--- app/src/main/java/com/example/newworldphotoeditor/MainActivity.java
+++ app/src/main/java/com/example/newworldphotoeditor/MainActivity.java
@@ -8,29 +8,58 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.graphics.Bitmap;
 import android.graphics.Camera;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+
+import com.facebook.FacebookSdk;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
     private static final int GALLERY_RESULT = 1;
     private static final int CAMERA_RESULT = 2;
     private static final String FILE_PROVIDER_AUTHORITY = "com.example.newworldphotoeditor";
     private static final int CAMERA_PERMISSION_CODE = 99;
-    private static final int STORAGE_PERMISSION_CODE = 100 ;
+    private static final int STORAGE_PERMISSION_CODE = 100;
     private String mCaptureImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try {
+            PackageInfo info = null;
+            try {
+                info = getPackageManager().getPackageInfo(
+                        "com.example.newworldphotoeditor",
+                        PackageManager.GET_SIGNATURES);
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+
     }
 
     public void onCollage(View view) {
+        Intent intent = new Intent(MainActivity.this, LoginFacebookActivity.class);
+        startActivity(intent);
 
     }
 
